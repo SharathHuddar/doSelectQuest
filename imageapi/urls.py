15f7665d13d323b46465655170d5dc2eab346745
Sharath Huddar <sharath.huddar@gmail.com>
2017-05-10 19:36:47 +0530

    Basic file upload done

--- imageapi/urls.py
+++ imageapi/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import url, include
+from django.conf.urls.static import static
+from django.conf import settings
+from imageapi import views
+urlpatterns = [
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^imagedetail/$', views.ImageDetail.as_view()),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 from rest_framework.authtoken import views
 
-urlpatterns = [
+urlpatterns += [
     url(r'^api-token-auth/$', views.obtain_auth_token)
 ]
--- imageapi/views.py
+++ imageapi/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+from rest_framework import generics
+# from imageapi.serializers import  UserSerializer
+from rest_framework.views import APIView
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+class UserList(APIView):
+    authentication_classes = (TokenAuthentication, SessionAuthentication)
+    permission_classes = (IsAuthenticated,)
+    def get(self, request):
+        content = {
+            'user': str(request.user),
+            'auth': str(request.auth)
+        }
+        return Response(content)
+        
+class ImageDetail(APIView):
+    authentication_classes = (TokenAuthentication, SessionAuthentication)
+    permission_classes = (IsAuthenticated,)
+    
+    def post(self, request):
+        myfile = request.FILES['myfile']
+        print (request.user.username)
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        content = {
+            'uploaded_file_url': fs.url(filename)
+        }
+        return Response(content)

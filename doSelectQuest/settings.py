5bed816e142481f7c9250232a6c2dc102119dfe5
Sharath Huddar <sharath.huddar@gmail.com>
2017-05-11 17:10:40 +0530

    Minor change

--- doSelectQuest/settings.py
+++ doSelectQuest/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p@gkh4*#(%j4-2oo8y4j9+4agyj0cqyxo*)sf&hl*_9rb$oyj&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
